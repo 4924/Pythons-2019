10a96f9da156f885f1fc51247f4321fc051457e0
Detestedwharf16 <Hbrock2019@gmail.com>
2018-09-18 17:10:11 -0400

    Add files via upload

---
+++ Build-A-Bot Workshop/src/Robot.cpp
@@ -0,0 +1,103 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <Drive/DifferentialDrive.h>
+#include <IterativeRobot.h>
+#include <Joystick.h>
+#include <Spark.h>
+#include <wpilib.h>
+#include <XboxController.h>
+#include <DoubleSolenoid.h>
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class.
+ * Runs the motors with arcade steering.
+ */
+class Robot : public frc::IterativeRobot {
+
+	//drivetrain setup
+	frc::Spark m_frontLeft{0};
+	frc::Spark m_rearLeft{0};
+	frc::SpeedControllerGroup m_left{m_frontLeft, m_rearLeft};
+
+	frc::Spark m_frontRight{1};
+	frc::Spark m_rearRight{1};
+	frc::SpeedControllerGroup m_right{m_frontRight, m_rearRight};
+
+	frc::DifferentialDrive m_drive{m_left, m_right};
+	//drivetrain setup
+
+	//pnuematics
+	frc::Compressor Compressor1 {0};
+	frc::DoubleSolenoid L_Solenoid {0,1};
+	frc::DoubleSolenoid R_Solenoid {2,3};
+	//pnuematics
+
+	//setup joysticks
+	frc::Joystick m_stick{1};
+	Joystick m_Xbox{0};
+	//setup joysticks
+
+	//Random variables
+	int tankDrive = -1;
+	bool toggled;
+	bool toggled2;
+	float speed;
+	//Random variables
+
+public:
+
+	void TeleopInit() {
+		Compressor1.SetClosedLoopControl(true);
+
+	}
+
+	void TeleopPeriodic() {
+
+		//pnuematics shenanigans
+		if (m_Xbox.GetRawButtonPressed(2) and toggled2 == false) {
+			toggled2 = true;
+			R_Solenoid.Set(frc::DoubleSolenoid::Value::kForward);
+			L_Solenoid.Set(frc::DoubleSolenoid::Value::kForward);
+		} else if (m_Xbox.GetRawButtonReleased(2)) {
+			toggled2 = false;
+			R_Solenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+			L_Solenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+		}
+		//pnuematics shenanigans
+
+		//speed set
+		speed = m_Xbox.GetRawAxis(3) + 0.5;
+		if (speed > 1){
+			speed = 1;
+		} else if (speed < -1) {
+			speed = -1;
+		}
+		//speed set
+
+		//tank/arcade toggling
+		if (m_Xbox.GetRawButtonPressed(1) and toggled == false) {
+			tankDrive *= -1;
+			toggled = true;
+		} else if (m_Xbox.GetRawButtonReleased(1)) {
+			toggled = false;
+		}
+		//tank/arcade toggling
+
+		//drive!
+		if (tankDrive==-1) {
+			m_drive.ArcadeDrive(-m_Xbox.GetRawAxis(1) * speed, m_Xbox.GetRawAxis(0) * speed);
+		} else if (tankDrive==1){
+			m_drive.TankDrive(-m_Xbox.GetRawAxis(1) * speed, -m_Xbox.GetRawAxis(5)* speed);
+		}
+		//drive!
+	}
+
+
+};
+
+START_ROBOT_CLASS(Robot)
