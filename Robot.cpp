70073e9965191b89eaf5cc6a58315e5b3219e28b
Drewlonger <drewlonger12@gmail.com>
2019-03-12 09:17:31 -0400

    Update
    Code from Monday.

--- Robot.cpp
+++ Robot.cpp
@@ -5,12 +5,13 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include <Drive/DifferentialDrive.h>
-#include <IterativeRobot.h>
-#include <Joystick.h>
-#include <Spark.h>
-#include <wpilib.h>
-#include <Controller.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/IterativeRobot.h>
+#include <frc/Joystick.h>
+#include <frc/Spark.h>
+#include <frc/WPILib.h>
+#include <frc/Controller.h>
+#include <cameraserver/CameraServer.h>
 #include <cstdlib>
 #include <iostream>
 
@@ -19,7 +20,7 @@
  * This is a demo program showing the use of the DifferentialDrive class.
  * Runs the motors with arcade steering.
  */
-class Robot : public frc::IterativeRobot {
+class Robot : public frc::IterativeRobot{
 
 	//drivetrain setup
 	frc::Spark m_frontLeft{0};
@@ -35,35 +36,60 @@ class Robot : public frc::IterativeRobot {
 	frc::Spark m_leftArmMotor{4};
 	frc::Spark m_rightArmMotor{5};
 	frc::Spark m_intakeMotor{6};
+
+	frc::Joystick m_Xbox{0};
+
+	//frc::StartRobot<Robot>();
+
 	//drivetrain setup
 
 	//setup joysticks
-	//Joystick::Joystick(0);
 
-	frc::Joystick m_stick{1};
-	Joystick m_Xbox{0};
+/*Joystick m_Xboxjoystick
+
+public:
+     Robot(){
+     }
+     void RobotInit() {
+       m_Xboxjoystick = new Joystick(1);
+     }
+*/
+
+	//Joystick(m_Xbox){0}
+	//frc::Joystick m_stick{1};
+
 	//setup joysticks
 
 	//Random variables
 	int tankDrive = -1;
 	bool toggled;
 	bool toggled2;
-	float accel;
-	float max;
-	float speed;
-	float speedH;
+	float accelstr;
+	float acceltrn;
+	float maxstr;
+	float maxtrn;
+	float speed; // straight
+	float speedF;
+	float speedB;
+	float speedtrn;
+	float turning;
+	float turns;
+	float turningL;
+	float turningR;
 	float armSpeed;
 	float intakeSpeed;
+	float turningspeedL;
+	float turningspeedR;
 	//Random variables
 
 private:
 
 	void RobotInit()
 	{
-		/*CameraServer::GetInstance()->StartAutomaticCapture(0);
-		CameraServer::GetInstance()->StartAutomaticCapture(1);*/
+		CameraServer::GetInstance()->StartAutomaticCapture(0);
+		CameraServer::GetInstance()->StartAutomaticCapture(1);
 	}
-
+	
 
 public:
 
@@ -74,42 +100,91 @@ public:
 
 	void TeleopPeriodic() {
 		//speed set
-		speedH = 0.7;
+		speed = 0;
+		speedtrn = 0.01;
 
-		accel = 0.05;
-		max = abs(m_Xbox.GetRawAxis(1));
+		accelstr = 0.01;  //.05 last time
+		maxstr = abs(m_Xbox.GetRawAxis(1));
 
 		if (m_Xbox.GetRawAxis(1) > 0.1) {
-			speed = speed + accel;
+			speed = -m_Xbox.GetRawAxis(1) + accelstr; 
 		} else if (m_Xbox.GetRawAxis(1) < -0.1) {
-			speed = speed - accel;
-		} else if (m_Xbox.GetRawAxis(1) > -0.1 and m_Xbox.GetRawAxis(1) < 0.1) {
-			if (speed > 0) {
-				speed = speed - accel;
-			} else if (speed < 0) {
-				speed = speed + accel;
-			}
+			speed = -m_Xbox.GetRawAxis(1) - accelstr;
+		} else{
+			speed = 0;
+		}
+		//else if (m_Xbox.GetRawAxis(1) > 0.1 and m_Xbox.GetRawAxis(1) < -0.05) {
+			//if (speed > 0) {
+				//speed = speedF - accel;
+			//} else if (speed < 0) {
+				//speed = speedF + accel;
+			//}
+		//}
+
+		if (m_Xbox.GetRawAxis(0) > 0.01) {
+			turningR = m_Xbox.GetRawAxis(0) + speedtrn;
+		} else if (m_Xbox.GetRawAxis(0) < -0.01) {
+			turningL = -m_Xbox.GetRawAxis(0) + speedtrn;
+		} else{
+			turns = 0;
+			turningR = 0;
+			turningL = 0;
 		}
 
-		if (speed > max) {
-			speed = max;
-		} else if (speed < -max) {
-			speed = -max;
+		if (m_Xbox.GetRawAxis(3) > 0.01) {
+			turns = turns * 2;
+		} else if (m_Xbox.GetRawAxis(3) < 0.01) {
+			turns = turns * 1;
+		} else if (m_Xbox.GetRawAxis(2) > 0.01) {
+			turns = turns * 0.5;
+		} else if (m_Xbox.GetRawAxis(2) < -0.01) {
+			turns = turns * 1;
+		}
+		
+		/*if (speed > maxstr) {
+			speed = maxstr;
+		} else if (speed < -maxstr) {
+			speed = -maxstr;
+		}*/
+		/*if (m_Xbox.GetRawAxis(2) > -0.1) {
+			turningR = m_Xbox.GetRawAxis(2) * speed;
 		}
 
+		// else if (m_Xbox.GetRawAxis(2) < 0.1) {
+			//turningR = m_Xbox.GetRawAxis(2) * -speed;
+		//} 
+			if (m_Xbox.GetRawAxis(3) > -0.1) {	
+				turningL = m_Xbox.GetRawAxis(3) * speed;
+				}
 
-		/*
-		 if (speed > 1) {
-
-			speed = 1;
-		}
-
-		else if (speed < 1) {
-			speed = -1;
-		}
+			//	 else if (m_Xbox.GetRawAxis(3) < 0.1) {
+		//	turningL = m_Xbox.GetRawAxis(3) * -speed;
+	//	}
 		*/
+	if (turningR > turningL) {
+		turns = turningR;
+	} else if (turningL > turningR) {
+		turns = -turningL;
+	}
 
-		armSpeed = -0.6;
+/*
+acceltrn = 0.02;  //.05 last time
+		maxtrn = abs(m_Xbox.GetRawAxis(0));
+
+if (m_Xbox.GetRawAxis(0) > -0.1) {
+			turning = turningspeedL + acceltrn;
+		} else if (m_Xbox.GetRawAxis(1) < -0.1) {
+			turning = turningspeedR - acceltrn;
+	
+
+		if (turning > maxtrn) {
+			turning = maxtrn;
+		} else if (turning < -maxtrn) {
+			turning = -maxtrn;
+		}
+*/
+
+		armSpeed = -0.6 ;
 		intakeSpeed = 0;
 		//speed set
 
@@ -124,9 +199,9 @@ public:
 
 		//drive!
 		if (tankDrive==-1) {
-			m_drive.ArcadeDrive(-speed, m_Xbox.GetRawAxis(3) * speedH);
+			m_drive.ArcadeDrive(speed, turns);
 		} else if (tankDrive==1){
-			m_drive.TankDrive(m_Xbox.GetRawAxis(0) * speedH, m_Xbox.GetRawAxis(1), m_Xbox.GetRawButton(6));
+			m_drive.TankDrive(m_Xbox.GetRawAxis(0) * speed, m_Xbox.GetRawAxis(1), m_Xbox.GetRawButton(6));
 		}
 		//drive!
 
@@ -140,13 +215,14 @@ public:
 		}
 
 		if (m_Xbox.GetRawButtonPressed(6)) {
-			m_intakeMotor.Set(intakeSpeed -0.25);
+			m_intakeMotor.Set(intakeSpeed -0.7);
 		} else if (m_Xbox.GetRawButtonReleased(6)) {
 			m_intakeMotor.Set(intakeSpeed);
 		}
 	}
-
-
+int main()
+{
+	return 0;
 };
-
+};
 START_ROBOT_CLASS(Robot)
